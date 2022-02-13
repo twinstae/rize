60aa8941c2286c336e3b1eb3003354ce177a1170
twinstae <rabolution@gmail.com>
2022-02-13 18:12:51 +0900

    chore: 불필요한 테스트, 설정 파일 정리

--- react-tauri/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/아이즈원 메일/i);
-  expect(linkElement).toBeInTheDocument();
-});
