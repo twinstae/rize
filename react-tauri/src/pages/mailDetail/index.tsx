82d9e416e0154c7f7c22b1430103914f32f46e4a
twinstae <rabolution@gmail.com>
2022-02-25 19:46:01 +0900

    feat: 메일 상세에 헤더 인라인

--- react-tauri/src/pages/mailDetail/index.tsx
+++ react-tauri/src/pages/mailDetail/index.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Button, Divider } from "@chakra-ui/react";
 import MailBody from "../../components/MailBody";
-import MailListItem from "../../components/MailListItem";
 import { useDependencies } from "../../hooks/Dependencies";
 import { withSuspense } from "../../hooks/util";
 import useMailList from "../../mailList/useMailList";
+import styled from "@emotion/styled";
+import ProfileImage from "../../components/ProfileImage";
+
+const Title = styled.h3`
+  padding: 0;
+  margin: 0.25rem 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-weight: 500;
+`;
+
+const Wrapper = styled.header`
+  padding: 0.5rem;
+`;
 
 function MailDetailPage() {
-  const { navigation } = useDependencies();
+  const { navigation, toNick } = useDependencies();
   const mailId = navigation.params().id!;
-  const Link = navigation.Link;
 
   const mail = useMailList()
     .mailList("all")
@@ -18,13 +33,34 @@ function MailDetailPage() {
   const mailBody = mailBodyQuery.data;
 
   return (
-    <section>
-      <Link to={mail ? "/?mailId=" + mail.id : "/"}>
-        <span>돌아가기</span>
-      </Link>
-      <header>{mail ? <MailListItem mail={mail} hide={true} /> : null}</header>
+    <Wrapper>
+      <Button
+        colorScheme="izone"
+        leftIcon={<ArrowBackIcon />}
+        onClick={() => navigation.navigate(mail ? "/?mailId=" + mail.id : "/")}
+      >
+        돌아가기
+      </Button>
+
+      <Wrapper>
+        {mail ? (
+          <>
+            <ProfileImage member={mail.member} />
+            <span style={{ fontWeight: 500 }}>{toNick(mail.member)} </span>
+            <span
+              style={{
+                color: "darkgray",
+              }}
+            >
+              {mail.time}
+            </span>
+            <Title>{mail.subject}</Title>
+          </>
+        ) : null}
+      </Wrapper>
+      <Divider />
       {mailBody && <MailBody mailBody={mailBody} />}
-    </section>
+    </Wrapper>
   );
 }
 
