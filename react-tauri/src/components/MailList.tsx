3d84f5ce398dcfb0903a52b852c4769640f72066
twinstae <rabolution@gmail.com>
2022-02-25 19:47:41 +0900

    fix: MailList를 UnorderedList로 감싸기

--- react-tauri/src/components/MailList.tsx
+++ react-tauri/src/components/MailList.tsx
@@ -4,6 +4,7 @@ import { List } from "react-virtualized";
 import { withSuspense } from "../hooks/util";
 import useMailList from "../mailList/useMailList";
 import { useSearchParams } from "react-router-dom";
+import { UnorderedList } from "@chakra-ui/react";
 
 interface Props {
   mode: TabMode;
@@ -16,21 +17,23 @@ function MailList({ mode }: Props) {
 
   const getIndex = (mailList: MailT[]) =>
     Math.min(
-      mailList.findIndex((mail) => mailId === mail.id) + 5,
+      mailList.findIndex((mail) => mailId === mail.id) + 4,
       mailList.length - 1
     );
 
   return data ? (
-    <List
-      width={435}
-      height={580}
-      rowCount={data.length}
-      rowHeight={110}
-      scrollToIndex={getIndex(data)}
-      rowRenderer={({ key, index, style }) => (
-        <MailListItem key={key} mail={data[index]} style={style} />
-      )}
-    />
+    <UnorderedList padding={0} margin={0}>
+      <List
+        width={435}
+        height={580}
+        rowCount={data.length}
+        rowHeight={110}
+        scrollToIndex={getIndex(data)}
+        rowRenderer={({ key, index, style }) => (
+          <MailListItem key={key} mail={data[index]} style={style} />
+        )}
+      />
+    </UnorderedList>
   ) : null;
 }
 
