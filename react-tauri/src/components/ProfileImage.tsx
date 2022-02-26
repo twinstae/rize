8f17c14ae62bea319941c611584b9c31f9423eeb
twinstae <rabolution@gmail.com>
2022-02-26 21:26:30 +0900

    feat: ProfileImage의 size variants 추가

--- react-tauri/src/components/ProfileImage.tsx
+++ react-tauri/src/components/ProfileImage.tsx
@@ -1,25 +1,32 @@
 import React from "react";
-import { nameToNumberDict } from "../constants";
+import { MEMBER_LIST, nameToNumberDict } from "../constants";
 import { useDependencies } from "../hooks/Dependencies";
 
 interface Props {
   member: string;
+  size: "base" | "sm";
 }
 
 function getPath(member: string) {
+  if (member === "") return "img/izone-logo.png";
   if (member === "운영팀") return "img/izone-logo.png";
 
   return `img/profile/${nameToNumberDict[member]}.jpg`;
 }
 
-const ProfileImage: React.FC<Props> = ({ member }) => {
+const sizes = {
+  base: "3rem",
+  sm: "1.5rem",
+};
+
+const ProfileImage: React.FC<Props> = ({ member, size = "base" }) => {
   const { Image } = useDependencies();
 
   return (
     <Image
       path={getPath(member)}
       style={{
-        width: "3rem",
+        width: sizes[size],
         float: "left",
         borderRadius: "50%",
         marginRight: "1rem",
