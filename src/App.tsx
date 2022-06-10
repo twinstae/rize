02eae28dc26963fcb9804d15be818ff345cb5bfb
Taehee Kim <taehee.kim@sap.com>
2022-06-11 01:19:20 +0900

    feat: extract initPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { Button } from '@chakra-ui/react';
 import React from 'react';
-import { Route,Routes } from 'react-router-dom';
+import { Route,Routes, useLocation } from 'react-router-dom';
 
 import Config from './pages/Config';
+import InitPage from './pages/InitPage';
 import MailDetailPage from './pages/MailDetailPage';
 import MailListPage from './pages/MailListPage';
 import paths from './router/paths';
-import useNavigation from './router/useNavigation';
-import Test from './test/Test';
 
-const Init = () => {
-  const { Link } = useNavigation();
+const App = () => {
+  const location = useLocation();
   return (
-    <div>
-
-      <Link to={paths.TEST}>
-        <Button>test</Button>
-      </Link>
-    </div>
+    <Routes location={location} key={location.pathname}>
+      <Route path={paths.CONFIG} element={<Config />} />
+      <Route path={paths.MAIL_LIST} element={<MailListPage />} />
+      <Route path={paths.MAIL_DETAIL} element={<MailDetailPage />} />
+      <Route path={paths.ROOT} element={<InitPage />} />
+    </Routes>
   );
 };
-
-const App = () => (
-  <Routes>
-    <Route path={paths.CONFIG} element={<Config />} />
-    <Route path={paths.MAIL_LIST} element={<MailListPage />} />
-    <Route path={paths.MAIL_DETAIL} element={<MailDetailPage />} />
-    <Route path={paths.TEST} element={<Test />} />
-    <Route path={paths.ROOT} element={<MailListPage />} />
-  </Routes>
-);
 
 export default App;
