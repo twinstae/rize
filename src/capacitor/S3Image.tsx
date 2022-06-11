766664f31acb6ff5b09b032154c65320efff66bf
Taehee Kim <taehee.kim@sap.com>
2022-06-11 12:06:25 +0900

    feat: S3Image use downloaded file

--- src/capacitor/S3Image.tsx
+++ src/capacitor/S3Image.tsx
@@ -1,15 +1,35 @@
+import { Capacitor } from '@capacitor/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Http } from '@capacitor-community/http';
 import { Image } from '@chakra-ui/react';
 import React from 'react';
+import { useQuery } from 'react-query';
 
 import { ImageProps } from '../components/MockImage';
 
-
 const ROOT = 'https://rize-taehee-kim.s3.ap-northeast-2.amazonaws.com/';
+const downloadFile = async (path: string): Promise<string> => {
+  const result = await Filesystem.stat({
+    path: 'output/'+path,
+    directory: Directory.Cache
+  }).catch(() => null);
+  if(result){
+    return Capacitor.convertFileSrc(result.uri);
+  }
+  return Http.downloadFile({
+    url: encodeURI(ROOT + path),
+    filePath: 'output/'+path,
+    fileDirectory: Directory.Cache,
+    method: 'GET',
+  }).then(() => downloadFile(path));
+};
+
 const S3Image: React.FC<ImageProps> = ({ path, style, width }) => {
+  const { data: src } = useQuery<string, Error>(['image', path], () => downloadFile(path));
   return (
     <Image
       borderRadius='full'
-      src={ROOT + path}
+      src={src ?? `https://via.placeholder.com/${width}`}
       width={width}
       style={style}
     />
