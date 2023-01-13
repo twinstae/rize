57303f8c783a12deafc5924afcee3acdad9ad28f
Taehee Kim <rabolution@gmail.com>
2023-01-13 23:42:13 +0900

    feat: ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React, { Suspense } from 'react';
 import { Stack } from './router/useStatckNavigation';
 import QueryWrapper, { JotaiQueryWrapper } from './hooks/QueryWrapper';
 import InitPage from './pages/InitPage';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App = () => {
   return (
     <QueryWrapper>
       <JotaiQueryWrapper>
-        <Suspense fallback={ <InitPage />}>
-          <Stack />
+        <Suspense fallback={<InitPage />}>
+          <ErrorBoundary fallback={({ error }) => (
+            <div className="p-4 ring-1 ring-error w-screen">
+              <h2 className="text-xl">{error.name}: {error.message}</h2>
+              <p className="text-xs" style={{ wordWrap: 'break-word' }}>{error.stack}</p>
+            </div>
+          )}>
+            <Stack />
+          </ErrorBoundary>
         </Suspense>
       </JotaiQueryWrapper>
     </QueryWrapper>
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component<{ fallback: React.FC<{ error: Error }>, children: React.ReactNode }> {
+  state: {hasError: boolean, cause?: Error };
+  constructor(props: { fallback: React.FC<{ error: Error }>, children: React.ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, cause: error };
+  }
+  
+  render() {
+    if (this.state.hasError && this.state.cause) {
+      // You can render any custom fallback UI
+      const Fallback = this.props.fallback;
+      return <Fallback error={this.state.cause} />;
+    }
+  
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
