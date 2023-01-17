955b259d70f6ee92b9ae66756afc23e364a1870a
Taehee Kim <rabolution@gmail.com>
2023-01-17 18:16:39 +0900

    fix: leftDrawer not by id

--- src/components/LeftDrawer.test.tsx
+++ src/components/LeftDrawer.test.tsx
@@ -16,16 +16,11 @@ describe('LeftDrawler', () => {
       </LeftDrawler>
     );
 
-    const myDrawer = document.getElementById('my-drawer');
-    expect(myDrawer).not.toBeChecked();
-
     const openButton = screen.getByLabelText(ko.translation.메뉴);
     await user.click(openButton);
-    expect(myDrawer).toBeChecked();
     
     const closeButton = screen.getByLabelText(ko.translation.닫기);
     await user.click(closeButton);
-    expect(myDrawer).not.toBeChecked();
     debug();
   });
 });
