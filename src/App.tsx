d256ce19b44734fa6d3d2b7eee37c270dcb79040
Taehee Kim <rabolution@gmail.com>
2023-01-17 14:25:09 +0900

    fix: remove unused suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Stack } from './router/useStatckNavigation';
 import QueryWrapper, { JotaiQueryWrapper } from './hooks/QueryWrapper';
-import InitPage from './pages/InitPage';
 import ErrorBoundary from './components/ErrorBoundary';
 
 function ErrorFallback({ error }: { error: Error}){
@@ -10,6 +9,7 @@ function ErrorFallback({ error }: { error: Error}){
       <h2 className="text-xl">{error.name}: {error.message}</h2>
       <p className="text-xs" style={{ wordWrap: 'break-word' }}>{error.stack}</p>
       <button className="btn btn-primary" onClick={() => {
+
         location.reload();
       }}>앱 다시 시작하기</button>
     </div>
@@ -20,11 +20,9 @@ function App(){
   return (
     <QueryWrapper>
       <JotaiQueryWrapper>
-        <Suspense fallback={<InitPage />}>
-          <ErrorBoundary fallback={ErrorFallback}>
-            <Stack />
-          </ErrorBoundary>
-        </Suspense>
+        <ErrorBoundary fallback={ErrorFallback}>
+          <Stack />
+        </ErrorBoundary>
       </JotaiQueryWrapper>
     </QueryWrapper>
   );
