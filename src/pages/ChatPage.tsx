8848b39dd46cbce8b826b1ee6679bbb47ab52176
Taehee Kim <rabolution@gmail.com>
2023-01-19 18:03:03 +0900

    feat: ChatGroup

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProfileImage from '../components/ProfileImage';
-import { VStack } from '../components/rize-ui';
+import { Button, VStack } from '../components/rize-ui';
 
 function Chat({
 	name,
@@ -20,7 +20,7 @@ function Chat({
 	last?: boolean;
 }) {
 	return (
-		<div className={`chat chat-${position} ${last ? 'chat-last' : ''} p-0`}>
+		<li className={`chat chat-${position} ${last ? 'chat-last' : ''} p-0`}>
 			<div className="chat-image avatar">
 				<div className="w-8 h-8">{first && <ProfileImage member={image} size="sm" />}</div>
 			</div>
@@ -33,27 +33,96 @@ function Chat({
 			<div className={`chat-bubble ${position === 'start' ? 'bg-purple-600 text-white' : 'bg-white text-black'}`}>
 				{message}
 			</div>
-		</div>
+		</li>
+	);
+}
+
+function ChatGroup({
+	name,
+	image,
+	time,
+	messages,
+	position,
+}: {
+	name: string;
+	image: string;
+	time: Date;
+	messages: string[];
+	position?: 'start' | 'end';
+}) {
+	return (
+		<VStack className="gap-1">
+			{messages.map((message, i) => (
+				<Chat
+					key={message+i}
+					name={name}
+					image={image}
+					time={time}
+					message={message}
+					first={i === 0}
+					last={i === messages.length - 1}
+					position={position}
+				/>
+			))}
+		</VStack>
+	);
+}
+
+function ChatForm({ sendMessage }: { sendMessage: (message: string) => void }) {
+	const [messageInput, setMessageInput] = useState<string>('');
+	return (
+		<form
+			className="w-full flex gap-1"
+			onSubmit={(e) => {
+				e.preventDefault();
+				sendMessage(messageInput);
+				setMessageInput('');
+			}}
+		>
+			<input
+				type="text"
+				className="input input-bordered flex-1"
+				name="message"
+				value={messageInput}
+				onChange={(event) => {
+					setMessageInput(event.currentTarget.value);
+				}}
+			/>
+			<Button type="submit">보내기</Button>
+		</form>
 	);
 }
 
 function ChatPage() {
+	const [wizoneMessages, setWizoneMessages] = useState<string[]>([]);
+
 	return (
-		<VStack className="p-2 h-screen w-full bg-purple-200 gap-1">
+		<VStack className="p-2 h-screen w-full bg-purple-200 gap-2">
 			<div className="text-center">
 				<span className="badge bg-slate-400 bg-opacity-60 text-white border-none text-sm p-3">오늘</span>
 			</div>
-			<Chat
+			<ChatGroup
 				name="장원영"
 				image="장원영"
-				message="그래두 만나서 너무 좋았어 위즈원ㅎㅎ"
 				time={new Date()}
-				first={true}
+				messages={[
+					'그래두 만나서 너무 좋았어 위즈원ㅎㅎ',
+					'기다려서 내가 더 보고 시펐징?',
+					'보고싶었는데 연락해서 좋았어',
+					'안녕',
+				]}
 			/>
-			<Chat name="장원영" image="장원영" message="기다려서 내가 더 보고 시펐징?" time={new Date()} />
-			<Chat name="장원영" image="장원영" message="보고싶었는데 연락해서 좋았어" time={new Date()} />
-			<Chat name="장원영" image="장원영" message="안녕" time={new Date()} last={true} />
-			<Chat name="위즈원" image="" message="오늘도 화이팅" time={new Date()} position="end" first={true} last={true} />
+			<ChatGroup name="위즈원" image="" messages={wizoneMessages} time={new Date()} position="end" />
+			<ChatForm
+				sendMessage={(message) => {
+					setWizoneMessages((old) => {
+						if (message) {
+							return [...old, message];
+						}
+						return old;
+					});
+				}}
+			/>
 		</VStack>
 	);
 }
