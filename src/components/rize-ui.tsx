bddecc80811458767768997f1ab972cdf77ca351
Taehee Kim <rabolution@gmail.com>
2023-01-19 15:26:17 +0900

    fix: ButtonComponent

--- src/components/rize-ui.tsx
+++ src/components/rize-ui.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import VirtualList from './VirtualStack';
+import type { RizeReactElement } from '../global';
 
 function FormLabel({ className, children, ...props }: React.ComponentProps<'label'>) {
 	return (
@@ -33,4 +34,24 @@ function HStack({ className, children, ...props }: React.ComponentProps<'div'>) 
 	);
 }
 
-export { FormLabel, Input, Radio, VStack, HStack, VirtualList };
+export type ButtonProps<T> = { as?: T; variant?: 'primary' | 'ghost'; size?: 'sm' | 'base'; circle?: '' | 'circle' };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Button = React.forwardRef(function Button<T extends RizeReactElement>({
+	as = 'button',
+	className,
+	variant = 'primary',
+	size = 'base',
+	circle = '',
+	children,
+	...props
+}: React.ComponentProps<T> & ButtonProps<T>, ref: React.Ref<HTMLElement>) {
+	const Element = as;
+	return (
+		<Element ref={ref} {...props} className={`btn btn-${variant} btn-${size} btn-${circle} ` + className}>
+			{children}
+		</Element>
+	);
+});
+
+export { FormLabel, Input, Radio, VStack, HStack, VirtualList, Button };
