c14f7473875c80f234bcc0b711965bc44cacbcde
Taehee Kim <rabolution@gmail.com>
2023-01-21 15:00:43 +0900

    fix: localhost

--- src/components/MockImage.tsx
+++ src/components/MockImage.tsx
@@ -7,5 +7,5 @@ export interface ImageProps extends React.ComponentProps<'img'> {
 }
 
 export const MockImage: React.FC<ImageProps> = ({ path, style, width, ...props }) => (
-	<img src={`http://192.168.219.107:8000/${path}`} style={style} alt="" width={width * 4} {...props} />
+	<img src={`http://localhost:8000/${path}`} style={style} alt="" width={width * 4} {...props} />
 );
--- src/tauri/index.tsx
+++ src/tauri/index.tsx
@@ -6,23 +6,20 @@ import { useColorMode, DependenciesWrapper } from '../hooks/Dependencies';
 import i18n from '../i18n/i18n';
 import { createUseMailList } from '../mailList/useMailList';
 import { useStackNavigation } from '../router/useStatckNavigation';
-// import fsJSON from './fsJSON';
-// import fsMailRepository from './fsMailRepository';
-// import fsStorageRepo from './fsStorageRepo';
-// import TauriImage from './TauriImage';
+import fsJSON from './fsJSON';
+import mailRepository from './fsMailRepository';
+import storageRepo from './fsStorageRepo';
+import Image from './TauriImage';
+import useProfileList from './useFsProfileList';
 import { useAtom } from 'jotai';
 import { isSplashEndAtom } from '../hooks/splashEndAtom';
-// import useFsProfileList from './useFsProfileList';
-import fakeMailRepository, { fakeFsJSON } from '../mailList/fakeMailRepository';
-import fakeStorageRepo from '../config/fakeStorageRepo';
-import { MockImage } from '../components/MockImage';
-import useProfileList from '../web/useProfileList';
+// import fakeMailRepository, { fakeFsJSON } from '../mailList/fakeMailRepository';
+// import fakeStorageRepo from '../config/fakeStorageRepo';
+// import { MockImage } from '../components/MockImage';
+// import useProfileList from '../web/useProfileList';
 
-const fsJSON = fakeFsJSON;
-const mailRepository = fakeMailRepository;
-const storageRepo = fakeStorageRepo;
+// const fsJSON = fakeFsJSON;
 const useMailList = createUseMailList(mailRepository);
-const Image = MockImage;
 
 storageRepo.getItem('lang').then((lang) => {
 	i18n.changeLanguage(lang);
