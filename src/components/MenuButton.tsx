73814d2b2735bf9994e956cb2394095a0560be18
Taehee Kim <rabolution@gmail.com>
2023-02-01 10:25:10 +0900

    feat: menu button hot key

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,17 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { useTranslation } from '../i18n/i18n';
 
 import { strs } from '../i18n/i18n';
 import HamBurgerIcon from './icons/HamBurgerIcon';
 import IconButtonWithTooltip from './IconButtonWithTooltip';
 import { drawlerContext } from './LeftDrawer';
+import { useHotkeys } from 'react-hotkeys-hook';
 
 function MenuButton() {
 	const { t } = useTranslation();
 	const { handleOpen } = useContext(drawlerContext);
+	const ref = useRef<HTMLButtonElement>(null);
 
+	useHotkeys('m', () => {
+		ref.current?.focus();
+		handleOpen();
+	});
 	return (
 		<IconButtonWithTooltip
+			ref={ref}
 			onClick={() => {
 				handleOpen();
 			}}
@@ -19,7 +26,7 @@ function MenuButton() {
 			direction="bottom"
 			className="drawer-button"
 			size="sm"
-			ariaLabel={t(strs.메뉴)}
+			ariaLabel={t(strs.메뉴)+'(m)'}
 		/>
 	);
 }
